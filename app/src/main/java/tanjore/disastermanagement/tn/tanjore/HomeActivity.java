45d5d576e4e391f08183897b2fff1e09084929e8
sibi <sibinehru99@gmail.com>
2018-08-02 23:12:03 +0530

    Modified by SIBI

--- app/src/main/java/tanjore/disastermanagement/tn/tanjore/HomeActivity.java
+++ app/src/main/java/tanjore/disastermanagement/tn/tanjore/HomeActivity.java
@@ -3,6 +3,8 @@ package tanjore.disastermanagement.tn.tanjore;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,24 +15,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tanjore.disastermanagement.tn.tanjore.IncidentHistoryRecyclerView.IncidentHistoryAdapter;
+
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    List<IncidentClass>fakeData = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //NAVIGATION DRAWER AND TOOLBAR
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,8 +42,24 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //RECYCLER VIEW
+        RecyclerView incidentHistory = findViewById(R.id.incident_history_list);
+        dummyData();
+        IncidentHistoryAdapter myAdapter = new IncidentHistoryAdapter(fakeData);
+        incidentHistory.setAdapter(myAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        incidentHistory.setLayoutManager(layoutManager);
     }
 
+    private void dummyData(){
+        for(int i=0;i<10;i++){
+            IncidentClass temp = new IncidentClass();
+            temp.incidentName = "Incident "+i;
+            temp.incidentTime = "Time "+i;
+            fakeData.add(temp);
+        }
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -80,17 +98,17 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.incident_report) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.incident_history) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.emergency_num) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.safety_tips) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.edit_account) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.delete_account) {
 
         }
 
